a9cfc269cc37c011adb856600db528dd9b41af93
Joy Lin <vvlnote@hotmail.com>
2019-07-29 01:17:27 +0000

    Automatically backed up by Learn

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -2,5 +2,16 @@ import React, { Component } from 'react';
 
 export default class EyesOnMe extends Component {
 
-  
+  handleFocus = () => {
+    console.log("Good!");
+  }
+
+  handleBlur = () => {
+    console.log("Hey! Eyes on me!");
+  }
+  render() {
+    return (
+      <button onFocus={this.handleFocus} onBlur={this.handleBlur}></button>
+    )
+  }
 }
